c5bee6d40513b7ef761201c727595a04625cbfef
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-12-18 14:43:55 +0200

    Add files via upload

---
+++ estuaryfhd-posterX-v8-mod_H-Ahmed.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+cd /tmp
+set -e 
+ wget "https://raw.githubusercontent.com/Ham-ahmed/Skins2024/refs/heads/main/estuaryfhd-posterX-v8-mod_H-Ahmed.tar.gz"
+wait
+tar -xzf estuaryfhd-posterX-v8-mod_H-Ahmed.tar.gz  -C /
+wait
+cd ..
+set +e
+rm -f /tmp/estuaryfhd-posterX-v8-mod_H-Ahmed.tar.gz
+sleep 2;
+echo "" 
+echo "" 
+echo "**************************************************************************************************"
+echo "#                   INSTALLED SUCCESSFULLY              #"
+echo "*                       ON - Panel                      *"
+echo "*                Enigma2 restart is required            *"
+echo "*************************************************************************************************"
+echo "               UPLOADED BY  >>>>   HAMDY_AHMED           "
+sleep 4;
+	echo '========================================================================================='
+###########################################                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "*************************************************************************************************"
+wait
+killall -9 enigma2
+exit 0
